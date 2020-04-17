ea2db42d423fd117af9a1d4e0817e4404ab2ada7
Aho Senpai <nicosenpai380@gmail.com>
2020-04-18 02:27:38 +1100

    Push

---
+++ Data-Ordo/Data-Ordo/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+// using System.Collections.Generic;
+// using System.ComponentModel;
+// using System.Drawing;
+// using System.Data;
+// using System.Text;
+using System.Windows.Forms;
+using Advanced_Combat_Tracker;
+// using System.IO;
+using System.Reflection;
+// using System.Xml;
+// using System.Text.RegularExpressions;
+
+[assembly: AssemblyTitle("Data-Ordo")]
+[assembly: AssemblyDescription("A Simple Plugin Try")]
+[assembly: AssemblyCompany("Aho Senpai")]
+[assembly: AssemblyVersion("0.0.0.1")]
+
+namespace Data_Ordo
+{
+    public class Main : UserControl, IActPluginV1
+    {
+
+    }   
+}
