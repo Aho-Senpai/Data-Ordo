5b9418c0ab0bcb0c7dda1baa725afd055dec8837
Aho Senpai <nicosenpai380@gmail.com>
2020-04-19 23:25:36 +1100

    UI and Checkbox

--- Data-Ordo/DataOrdo.cs
+++ Data-Ordo/DataOrdo.cs
@@ -17,161 +17,179 @@ namespace DataOrdo
 {
 	public class DataOrdoMain : UserControl, IActPluginV1
 	{
-		#region Designer Created Code (Avoid editing)
-		/// <summary> 
-		/// Required designer variable.
-		/// </summary>
-		private readonly System.ComponentModel.IContainer components = null;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private ToolStripSeparator toolStripSeparator1;
+        private CheckBox checkbox1;
+        private TextBox textBox1;
+        private StatusStrip statusStrip1;
 
-		/// <summary> 
-		/// Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
-
-		#region Component Designer generated code
-
-		/// <summary> 
-		/// Required method for Designer support - do not modify 
-		/// the contents of this method with the code editor.
-		/// </summary>
 		private void InitializeComponent()
 		{
-			this.label1 = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(3, 0);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(434, 13);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "This is the user interface that appears as a new tab under the Plugins tab.  [REP" +
-				"LACE ME]";
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(6, 16);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(431, 20);
-			this.textBox1.TabIndex = 1;
-			this.textBox1.Text = "Sample TextBox that has its value stored to the settings file automatically.";
-			// 
-			// PluginSample
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.label1);
-			this.Name = "PluginSample";
-			this.Size = new System.Drawing.Size(686, 384);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DataOrdoMain));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.checkbox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 362);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(686, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.StatusStrip1_ItemClicked);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripSeparator1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(686, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripSeparator1.MergeIndex = 2;
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 25);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(686, 337);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(678, 311);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Out Of Combat Logs";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(678, 311);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Encounters Logs";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // checkbox1
+            // 
+            this.checkbox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkbox1.AutoSize = true;
+            this.checkbox1.Location = new System.Drawing.Point(556, 3);
+            this.checkbox1.Name = "checkbox1";
+            this.checkbox1.Size = new System.Drawing.Size(98, 17);
+            this.checkbox1.TabIndex = 3;
+            this.checkbox1.Text = "Is NOT Parsing";
+            this.checkbox1.UseVisualStyleBackColor = true;
+            this.checkbox1.CheckedChanged += new System.EventHandler(this.checkbox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.textBox1.Location = new System.Drawing.Point(575, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // DataOrdoMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.checkbox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "DataOrdoMain";
+            this.Size = new System.Drawing.Size(686, 384);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
-		}
-
-		#endregion
-
-		private TextBox textBox1;
-
-		private System.Windows.Forms.Label label1;
-
-		#endregion
-		public DataOrdoMain()
+        }
+        public DataOrdoMain()
 		{
 			InitializeComponent();
 		}
-
-		Label lblStatus;    // The status label that appears in ACT's Plugin tab
-		readonly string settingsFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, "Config\\PluginSample.config.xml");
-		SettingsSerializer xmlSettings;
-
-		#region IActPluginV1 Members
 		public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
 		{
-			lblStatus = pluginStatusText;   // Hand the status label's reference to our local var
-			pluginScreenSpace.Controls.Add(this);   // Add this UserControl to the tab ACT provides
-			this.Dock = DockStyle.Fill; // Expand the UserControl to fill the tab's client space
-			xmlSettings = new SettingsSerializer(this); // Create a new settings serializer and pass it this instance
+            pluginScreenSpace.Controls.Add(this);
+			this.Dock = DockStyle.Fill;
 			LoadSettings();
-
-			// Create some sort of parsing event handler.  After the "+=" hit TAB twice and the code will be generated for you.
-			ActGlobals.oFormActMain.AfterCombatAction += new CombatActionDelegate(OFormActMain_AfterCombatAction);
-
-			lblStatus.Text = "Plugin Started";
 		}
 		public void DeInitPlugin()
 		{
-			// Unsubscribe from any events you listen to when exiting!
-			ActGlobals.oFormActMain.AfterCombatAction -= OFormActMain_AfterCombatAction;
-
 			SaveSettings();
-			lblStatus.Text = "Plugin Exited";
 		}
-		#endregion
-
-		void OFormActMain_AfterCombatAction(bool isImport, CombatActionEventArgs actionInfo)
-		{
-			throw new NotImplementedException();
-		}
-
 		void LoadSettings()
 		{
-			// Add any controls you want to save the state of
-			xmlSettings.AddControlSetting(textBox1.Name, textBox1);
-
-			if (File.Exists(settingsFile))
-			{
-				FileStream fs = new FileStream(settingsFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-				XmlTextReader xReader = new XmlTextReader(fs);
-
-				try
-				{
-					while (xReader.Read())
-					{
-						if (xReader.NodeType == XmlNodeType.Element)
-						{
-							if (xReader.LocalName == "SettingsSerializer")
-							{
-								xmlSettings.ImportFromXml(xReader);
-							}
-						}
-					}
-				}
-				catch (Exception ex)
-				{
-					lblStatus.Text = "Error loading settings: " + ex.Message;
-				}
-				xReader.Close();
-			}
 		}
 		void SaveSettings()
 		{
-			FileStream fs = new FileStream(settingsFile, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
-			XmlTextWriter xWriter = new XmlTextWriter(fs, Encoding.UTF8)
-			{
-				Formatting = Formatting.Indented,
-				Indentation = 1,
-				IndentChar = '\t'
-			};
-			xWriter.WriteStartDocument(true);
-			xWriter.WriteStartElement("Config");    // <Config>
-			xWriter.WriteStartElement("SettingsSerializer");    // <Config><SettingsSerializer>
-			xmlSettings.ExportToXml(xWriter);   // Fill the SettingsSerializer XML
-			xWriter.WriteEndElement();  // </SettingsSerializer>
-			xWriter.WriteEndElement();  // </Config>
-			xWriter.WriteEndDocument(); // Tie up loose ends (shouldn't be any)
-			xWriter.Flush();    // Flush the file buffer to disk
-			xWriter.Close();
 		}
-	}
+
+		private void StatusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+		{
+
+		}
+
+        private void checkbox1_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            if (cb.Checked == false)
+            {
+                checkbox1.Text = "Is NOT Parsing";
+            }
+            if (cb.Checked == true)
+            {
+                checkbox1.Text = "Is Parsing";
+                
+            }
+        }
+    }
 }
