fe50941082b4fea0946deb26360636a67e18f8f0
Aho Senpai <nicosenpai380@gmail.com>
2020-04-22 00:21:56 +1100

    Push

---
+++ Data-Ordo/DataOrdo/MainPlugin.cs
@@ -0,0 +1,40 @@
+﻿using Advanced_Combat_Tracker;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DataOrdo
+{
+    public class MainPlugin : IActPluginV1
+    {
+        public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
+        {
+            LoadSettings();
+        }
+
+        public void DeInitPlugin()
+        {
+            SaveSettings();
+        }
+
+        private void LoadSettings()
+        {
+
+        }
+
+        private void SaveSettings()
+        {
+
+        }
+    }
+}
---
+++ Data-Ordo/DataOrdo/UserInterfaceMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataOrdo
+{
+    public partial class UserInterfaceMain : UserControl
+    {
+        public UserInterfaceMain()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            if (cb.Checked == false)
+            {
+                checkBox1.Text = "Is NOT Parsing";
+            }
+            if (cb.Checked == true)
+            {
+                checkBox1.Text = "Is Parsing";
+
+            }
+        }
+    }
+}
