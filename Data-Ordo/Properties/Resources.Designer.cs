da45493ddd6e6c8514188c04caed9100d0ddb81a
Aho Senpai <nicosenpai380@gmail.com>
2020-04-21 13:48:38 +1100

    Commit

--- Data-Ordo/Properties/Resources.Designer.cs
+++ Data-Ordo/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Data_Ordo.Properties {
+namespace UserInterface.Properties {
     using System;
     
     
