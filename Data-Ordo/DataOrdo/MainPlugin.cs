94610d8bd5036cf7b20608738cb8befd6b2a25a3
Aho Senpai <nicosenpai380@gmail.com>
2020-04-22 23:16:18 +1100

    Update MainPlugin.cs

--- Data-Ordo/DataOrdo/MainPlugin.cs
+++ Data-Ordo/DataOrdo/MainPlugin.cs
@@ -15,37 +15,86 @@ using System.Xml;
 
 namespace DataOrdo
 {
-    public class MainPlugin : IActPluginV1
+    public class MainPlugin : UserControl, IActPluginV1
     {
         Label lblStatus;
+        SettingsSerializer xmlSettings;
+		string settingsFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, "Config\\Data-Ordo.config.xml");
 
         public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
         {
-            lblStatus = pluginStatusText;   // Hand the status label's reference to our local var
-			pluginScreenSpace.Controls.Add(this);   // Add this UserControl to the tab ACT provides
-			this.Dock = DockStyle.Fill; // Expand the UserControl to fill the tab's client space
-			xmlSettings = new SettingsSerializer(this); // Create a new settings serializer and pass it this instance
+			
+			lblStatus = pluginStatusText;			    // Hand the status label's reference to our local var
+			pluginScreenSpace.Controls.Add(this);		// Add this UserControl to the tab ACT provides
+			this.Dock = DockStyle.Fill;					// Expand the UserControl to fill the tab's client space
+			xmlSettings = new SettingsSerializer(this);	// Create a new settings serializer and pass it this instance
 			ActGlobals.oFormActMain.AfterCombatAction += new CombatActionDelegate(oFormActMain_AfterCombatAction); // Create some sort of parsing event handler.  After the "+=" hit TAB twice and the code will be generated for you.
-			lblStatus.Text = "Plugin Started";
-
 
 
             LoadSettings();
+
+            lblStatus.Text = "Crash Avoided!";
         }
 
         public void DeInitPlugin()
         {
+            ActGlobals.oFormActMain.AfterCombatAction -= oFormActMain_AfterCombatAction; // Unsubscribe from any events you listen to when exiting!
+
+
             SaveSettings();
+
+			lblStatus.Text = "Ready To Crash";
         }
+
+        void oFormActMain_AfterCombatAction(bool isImport, CombatActionEventArgs actionInfo)
+		{
+			throw new NotImplementedException();
+		}
 
         private void LoadSettings()
         {
+			if (File.Exists(settingsFile))
+			{
+				FileStream fs = new FileStream(settingsFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+				XmlTextReader xReader = new XmlTextReader(fs);
 
+				try
+				{
+					while (xReader.Read())
+					{
+						if (xReader.NodeType == XmlNodeType.Element)
+						{
+							if (xReader.LocalName == "SettingsSerializer")
+							{
+								xmlSettings.ImportFromXml(xReader);
+							}
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					lblStatus.Text = "Error loading settings: " + ex.Message;
+				}
+				xReader.Close();
+			}
         }
 
         private void SaveSettings()
         {
-
+            FileStream fs = new FileStream(settingsFile, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+			XmlTextWriter xWriter = new XmlTextWriter(fs, Encoding.UTF8);
+			xWriter.Formatting = Formatting.Indented;
+			xWriter.Indentation = 1;
+			xWriter.IndentChar = '\t';
+			xWriter.WriteStartDocument(true);
+			xWriter.WriteStartElement("Config");                // <Config>
+			xWriter.WriteStartElement("SettingsSerializer");    // <Config><SettingsSerializer>
+			xmlSettings.ExportToXml(xWriter);                   // Fill the SettingsSerializer XML
+			xWriter.WriteEndElement();                          // </SettingsSerializer>
+			xWriter.WriteEndElement();                          // </Config>
+			xWriter.WriteEndDocument();                         // Tie up loose ends (shouldn't be any)
+			xWriter.Flush();                                    // Flush the file buffer to disk
+			xWriter.Close();
         }
     }
 }
