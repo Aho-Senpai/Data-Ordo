c1d40a29776f0f49d005f1a23fea734258d24a41
Aho Senpai <nicosenpai380@gmail.com>
2020-04-22 20:49:48 +1100

    Update MainPlugin.cs

--- Data-Ordo/DataOrdo/MainPlugin.cs
+++ Data-Ordo/DataOrdo/MainPlugin.cs
@@ -17,8 +17,19 @@ namespace DataOrdo
 {
     public class MainPlugin : IActPluginV1
     {
+        Label lblStatus;
+
         public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
         {
+            lblStatus = pluginStatusText;   // Hand the status label's reference to our local var
+			pluginScreenSpace.Controls.Add(this);   // Add this UserControl to the tab ACT provides
+			this.Dock = DockStyle.Fill; // Expand the UserControl to fill the tab's client space
+			xmlSettings = new SettingsSerializer(this); // Create a new settings serializer and pass it this instance
+			ActGlobals.oFormActMain.AfterCombatAction += new CombatActionDelegate(oFormActMain_AfterCombatAction); // Create some sort of parsing event handler.  After the "+=" hit TAB twice and the code will be generated for you.
+			lblStatus.Text = "Plugin Started";
+
+
+
             LoadSettings();
         }
 
